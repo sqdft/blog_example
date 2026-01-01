9aafbc204b80167206ab5042e42c1bb58c3c92c3
Besty0728 <2120060205@qq.com>
2026-01-01 18:48:04 +0800

    加入类似于Antigravity的效果、新APP-提示词优化

--- src/config.ts
+++ src/config.ts
@@ -84,11 +84,18 @@ export const siteConfig: SiteConfig = {
 			description: "利用CloudflareWorker搭建的私人AI绘图",
 			external: true,
 		},
-				{
+		{
 			name: "私人AI提示词",
 			url: "https://aiprompt.ai0728.com.cn/",
 			image: "/favicon/aiprompt.webp",
 			description: "个人研究的较棒AI提示词集合",
+			external: true,
+		},
+		{
+			name: "提示词优化",
+			url: "https://prompt.micostar.cc",
+			image: "/favicon/prompts.webp",
+			description: "AI 提示词一键优化工具",
 			external: true,
 		}
 	]
